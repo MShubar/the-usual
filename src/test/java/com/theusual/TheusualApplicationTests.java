4f4c749b81fd33a23b5f46cfecf9bd0228b4ffcd
dev08 <dev08@saluberdev.com>
2025-10-20 13:12:08 +0300

    deployment

--- src/test/java/com/theusual/TheusualApplicationTests.java
+++ src/test/java/com/theusual/TheusualApplicationTests.java
@@ -2,12 +2,22 @@ package com.theusual;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+import com.theusual.services.AzureStorageService;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+    "azure.storage.account-name=test-account",
+    "azure.storage.account-key=test-key",
+    "azure.storage.container-name=test-container"
+})
 class TheusualApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private AzureStorageService azureStorageService;
 
+    @Test
+    void contextLoads() {
+    }
 }
